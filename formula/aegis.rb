766070e5c8d7eea4d7a538f0f59b392908a50b2e
Reza Handzalah <reza.handzalah@mbiz.co.id>
2022-09-27 00:15:10 +0700

    binary name

--- formula/aegis.rb
+++ formula/aegis.rb
@@ -3,11 +3,11 @@ require "formula"
 class Aegis < Formula
   desc "Convenient DMP aegis development."
   homepage "https://github.com/tbmreza/aegis-manager"
-  url "https://github.com/tbmreza/homebrew-aegis-manager/releases/download/v0.0.1/aegis-macos-x86_64-v0.0.1.tar.gz"
+  url "https://github.com/tbmreza/aegis-manager/releases/download/v0.0.1/aegis-macos-x86_64-v0.0.1.tar.gz"
   sha256 "b52c12da3562609c531bdb3538796bdfba17853c4847d7ac726f9a55b2929b0d"
   version "0.0.1"
 
   def install
-    bin.install "aegis"
+    bin.install "cli"
   end
 end
