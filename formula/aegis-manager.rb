f5c8ed0c896f70b945fa538416f3f0cec5a4e2f6
Reza Handzalah <reza.handzalah@mbiz.co.id>
2022-09-27 06:22:13 +0700

    rename formula

--- formula/aegis-manager.rb
+++ formula/aegis-manager.rb
@@ -4,10 +4,10 @@ class Aegis < Formula
   desc "Convenient DMP aegis development."
   homepage "https://github.com/tbmreza/aegis-manager"
   url "https://github.com/tbmreza/aegis-manager/releases/download/v0.0.1/aegis-macos-x86_64-v0.0.1.tar.gz"
-  sha256 "b52c12da3562609c531bdb3538796bdfba17853c4847d7ac726f9a55b2929b0d"
+  sha256 "5ab51f553663c5e61ccb333a1f9ff88b2ce356a3164aba2b0363bc8eadcacf3c"
   version "0.0.1"
 
   def install
-    bin.install "cli"
+    bin.install "aegis"  # name of uncompressed release executable
   end
 end
