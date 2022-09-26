8c208eed261484731a5318928f93d0c8335031e3
Reza Handzalah <reza.handzalah@mbiz.co.id>
2022-09-26 14:22:22 +0700

    use compressed binary

--- formula/aegis.rb
+++ formula/aegis.rb
@@ -3,16 +3,11 @@ require "formula"
 class Aegis < Formula
   desc "Convenient DMP aegis development."
   homepage "https://github.com/tbmreza/aegis-manager"
-  url "https://github.com/tbmreza/aegis-manager/releases/download/v0.0.1/aegis-macos-x86_64-v0.0.1"
-  sha256 "64d34fe2c8e8a7e064cd8fa132b246c1aeab871160d0c85a22d6b4026a186bf2"
-  head "https://github.com/tbmreza/aegis-manager.git"
+  url "https://github.com/tbmreza/homebrew-aegis-manager/releases/download/v0.0.1/aegis-macos-x86_64-v0.0.1.tar.gz"
+  sha256 "b52c12da3562609c531bdb3538796bdfba17853c4847d7ac726f9a55b2929b0d"
+  version "0.0.1"
 
   def install
     bin.install "aegis"
   end
-
-  # Homebrew requires tests.
-  # test do
-  #   assert_match "fed version 1.0.0", shell_output("#{bin}/fed -v", 2)
-  # end
 end
