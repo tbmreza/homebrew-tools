b38e5b1f956d34664333a3a7f63844e6543a71b0
Reza Handzalah <reza.handzalah@mbiz.co.id>
2022-10-03 08:33:01 +0700

    v0.0.2

--- formula/aegis-manager.rb
+++ formula/aegis-manager.rb
@@ -3,9 +3,9 @@ require "formula"
 class AegisManager < Formula
   desc "Convenient DMP aegis development."
   homepage "https://github.com/tbmreza/aegis-manager"
-  url "https://github.com/tbmreza/aegis-manager/releases/download/v0.0.1/aegis-macos-x86_64-v0.0.1.tar.gz"
-  sha256 "5ab51f553663c5e61ccb333a1f9ff88b2ce356a3164aba2b0363bc8eadcacf3c"
-  version "0.0.1"
+  url "https://github.com/tbmreza/aegis-manager/releases/download/v0.0.2/aegis-macos-x86_64-v0.0.2.tar.gz"
+  sha256 "608b98c57291729836fbae5f01f08247b515d85b2ffba317f0bef55f691a002d"
+  version "0.0.2"
 
   def install
     bin.install "aegis"  # name of uncompressed release executable
